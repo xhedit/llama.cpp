c238a5fc304c74b3b9aa4b443f5e99142f687134
xhedit <71542719+xhedit@users.noreply.github.com>
2024-03-11 12:54:31 -0400

    merge upstream

--- common/common.h
+++ common/common.h
@@ -81,6 +81,8 @@ struct gpt_params {
     llama_rope_scaling_type rope_scaling_type = LLAMA_ROPE_SCALING_TYPE_UNSPECIFIED;
     llama_pooling_type      pooling_type      = LLAMA_POOLING_TYPE_UNSPECIFIED; // pooling type for embeddings
 
+    std::string layer_order             = ""; // does nothing
+
     // // sampling parameters
     struct llama_sampling_params sparams;
 
